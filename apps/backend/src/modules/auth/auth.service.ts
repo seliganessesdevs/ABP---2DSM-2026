20381eae933593bce369e7f3924e79aa91461ef6
Lucas Cecon <124888722+Cesom-TheRunner@users.noreply.github.com>
2026-04-23 23:02:04 -0300

    feat(auth): implementa o auth service

---
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { AppError } from "../../errors/AppError";
+import { db } from '../../config/database';
+import { LoginDTO, LoginResponse } from "./auth.types";
+import { generateToken } from "../../utils/jwt.utils";
+import { comparePassword } from "../../utils/hash.util";
+
+export async function login(dto: LoginDTO): Promise<LoginResponse> {
+      const user = await db.user.findUnique({
+            where: { email: dto.email },
+      });
+
+      const isPasswordValid = await comparePassword(dto.password, user.password_hash);
+
+      if (!isPasswordValid) {
+            throw new AppError("Credenciais inválidas", 401);
+      }
+
+      const token = generateToken({ sub: user.id.toString(), role: user.role });
+
+      return {
+            token,
+            user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                  role: user.role,
+            },
+      };
+}
